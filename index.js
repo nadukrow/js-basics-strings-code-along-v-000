cb56bfc868d23237c68a4a1d3a658a8002d9ba9e
Nana Adu-Krow <nadukrow@gmail.com>
2018-04-18 02:22:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,4 +14,4 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = 'Welcome, ';
+const shortGreeting = 'Welcome, ${currentUser}[0]';
